75d117953052c0e1727bf30e9b417504b47cb9e5
Chase Cohen <chaserkeo@gmail.com>
2026-03-08 20:26:02 -0400

    Added new changes to the branch

--- NewCode.java
+++ NewCode.java
@@ -4,4 +4,11 @@ public static void main(String args[]) {
     for (int i = 0; i < 4; i++) {
         System.out.println("This file is cool.")
     }
+
+    int branchLoop = 10;
+    while (branchLoop > 0) {
+        System.out.println("This is the new loop, 
+            only in the branch.");
+        branchLoop--;
+    }
 }
