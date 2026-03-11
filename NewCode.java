7a63189ab8b3c6722b149ab6e415f3b820ed93e7
Chase Cohen <chaserkeo@gmail.com>
2026-03-10 21:38:43 -0400

    Added new branch variable

--- NewCode.java
+++ NewCode.java
@@ -1,4 +1,5 @@
 public static void main(String args[]) {
+    int branchVar = 1;
     System.out.println("New file!")
 
     for (int i = 0; i < 4; i++) {
@@ -10,5 +11,14 @@ public static void main(String args[]) {
         System.out.println("This is the new loop, 
             only in the branch.");
         branchLoop--;
+        branchVar *= 2;
+    }
+
+    if (branchVar > 12) {
+        System.out.println("This is a large branch
+            variable.");
+    } else {
+        System.out.println("This is a smaller branch
+            variables.");
     }
 }
